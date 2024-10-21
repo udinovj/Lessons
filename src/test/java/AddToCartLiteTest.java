1ac781a623dbcfce0e3d631d054622f2fb812387
Eduard <edu.muhovych@gmail.com>
2024-10-21 18:22:38 +0300

    firsttest

--- src/test/java/AddToCartLiteTest.java
+++
@@ -1,8 +0,0 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-
-
